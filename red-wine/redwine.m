0bbf05b19a4a48fb14b936d24774d3b1baa59f7a
Kaiwen Zheng (RIT Student) <kxz6582@ad.rit.edu>
2017-09-07 15:24:39 -0400

    Things happend.

--- red-wine/redwine.m
+++ red-wine/redwine.m
@@ -3,7 +3,7 @@ redWineData=load('winequality-red.csv');
 
 y = redWineData(:,12);
 %Two sample solutions below, neither is good enough for points:
-
+M=[redWineData(:,1:11) redWineData(:,1:3).^2 redWineData(:,7:11).^2 redWineData(:,1:3).^3 redWineData(:,7:11).^3 redWineData(:,9:11).^4];  %avgSqErr=0.3893
 
 
 %M=[redWineData(:,1:8).^6 redWineData(:,1:11).^0.5 redWineData(:,1:11).^0.0005];  %avgSqErr=0.3996
