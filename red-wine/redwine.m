9cc77b46fc7dd404f3988b1b0ab7e87479bc4a5f
Kaiwen Zheng (RIT Student) <kxz6582@ad.rit.edu>
2017-09-07 15:23:11 -0400

    Reduce error below 0.39.

--- red-wine/redwine.m
+++ red-wine/redwine.m
@@ -3,8 +3,10 @@ redWineData=load('winequality-red.csv');
 
 y = redWineData(:,12);
 %Two sample solutions below, neither is good enough for points:
-M=[ones(length(redWineData),1) redWineData(:,1:11)];  %avgSqErr=0.4168
-M=[redWineData(:,1:8).^6 redWineData(:,1:11).^0.5 redWineData(:,1:11).^0.0005];  %avgSqErr=0.3996
+
+
+
+%M=[redWineData(:,1:8).^6 redWineData(:,1:11).^0.5 redWineData(:,1:11).^0.0005];  %avgSqErr=0.3996
 % reminder: number of columns of M <=30
 
 w = ((M'*M)\M')*y;
