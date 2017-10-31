5c2c743287510b795acd96acc2ea2ebfeea530ec
KaiwenZ <kaiwen.zheng1939@gmail.com>
2017-10-31 09:03:56 -0400

    Working on question 2 hw7

---
+++ hw7/hw7.m
@@ -0,0 +1,29 @@
+clear;
+clc;
+close all;
+%==========================================================================
+% question 1
+%==========================================================================
+% a)What is the dimension of the input features?
+% ans: 3
+
+% b)Would this work better as a happy-neutral-sad or happy-sad classifier?
+% ans: happy-sad classifier
+
+% c)What is the dimension of the y(2)?
+% ans: 2
+
+% d)x0(1) and a0(2) are called?
+% ans: bias terms
+
+% e)If the softmax function is used for the output layer, how many nodes use activation functions?
+% ans: 3
+
+% f)If you had training samples, {xi,yi}, i=1...10,000, would you pre-train with an unsupervised technique?
+% ans: no
+
+
+
+%==========================================================================
+% question 2
+%==========================================================================
