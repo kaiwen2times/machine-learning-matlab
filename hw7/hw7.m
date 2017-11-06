61fa16007c890a530e5b9d96a3b746c17c757293
KaiwenZ <kaiwen.zheng1939@gmail.com>
2017-11-06 15:51:49 -0500

    working on question 8.

--- hw7/hw7.m
+++ hw7/hw7.m
@@ -53,3 +53,63 @@ close all;
 %==========================================================================
 % in proportion to input values, because all of the information we have is
 % the input values.
+
+
+
+%==========================================================================
+% question 6
+%==========================================================================
+% (yi - ai) is the difference between the prediction and the ground truth.
+% a * (1 - a) is for the activation function
+% xj is one of the input or a neuron.
+
+
+
+%==========================================================================
+% question 7
+%==========================================================================
+% a, d, f, i
+
+
+
+%==========================================================================
+% question 8
+%==========================================================================
+%Download hwk7files_forMycourses.zip and place them into an appropriate
+%hwk7 directory.  
+%Update the two paths below for your machine
+ 
+% We will use mnist hand written digits, '0' through '9'
+load('ex4data1.mat');  %5000 Mnist digits from Andrew Ng class
+n = size(X, 1);  %number of samples = 5000, 500 from each class
+D = size(X,2);  %number of dimensions/sample.  20x20=400
+C = length(unique(y));  %number of classes, class values are 1...10, where 10 is a digit '0'
+ 
+% Randomly select 100 data points to display
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+ 
+displayData(X(sel, :));  %This function is from Andrew Ng's online class
+ 
+%Convert X and y data into Matlab nnet format:
+inputs = X';
+%one-hot encoding ground truth values 
+targets = zeros(C,n);
+for ii=1:n
+  targets(y(ii),ii) =  1;
+end
+%If given one-hot encoding, can convert back to vector of ground truth
+%class values:
+% target1Dvector=zeros(n,1);
+% for ii=1:n
+%         target1Dvector(ii) = find(targets(:,ii) == 1);
+% end
+% max(target1Dvector-y) %this will be zero
+
+fprintf('\nLoading Saved Neural Network Parameters ...\n')
+ 
+% Load the weights into variables Theta1 and Theta2
+load('ex4weights.mat');  %Pre-learned weights from Andrew Ng class
+ 
+% Unroll parameters 
+nn_params = [Theta1(:) ; Theta2(:)];
