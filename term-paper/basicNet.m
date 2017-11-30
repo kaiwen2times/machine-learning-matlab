5d2971068af4c08f2c6d2d2b961bb6b353e21210
KaiwenZ <kaiwen.zheng1939@gmail.com>
2017-11-29 21:20:50 -0500

    fix an issue.

--- term-paper/basicNet.m
+++ term-paper/basicNet.m
@@ -1,19 +1,17 @@
 % specifying input and output
 X = [0 0 1; 0 1 1; 1 0 1; 1 1 1];
-y = [0,1,1,0]';
+y = [0,0,1,1]';
 
 rng(1); % seed the random number generator
 weights = rand(3,1); % there are only two hidden layers initialize one set of weights
 
-for i = 1:20
+iter = 100;
+for i = 1:iter
   layer1 = X;
   layer2 = layer1 * weights;
-  activation = sigmoid(layer2);
-  error = y - activation;
-  err_sum(i) = sum(error);
+  act = sigmoid(layer2);
+  error = y - act
   % (GT - prediction) * x * a * (1-a)
-  delta = layer1' * error * (activation' * (1-activation));
+  delta = layer1' * (error .* (act .* (1-act)));
   weights = weights + delta;
 end
-
-plot(1:20,err_sum)
