2b55490756b64472414bed2324e222db98875be9
KaiwenZ <kaiwen.zheng1939@gmail.com>
2017-12-01 09:33:53 -0500

    plot is ready.

--- term-paper/basicNet.m
+++ term-paper/basicNet.m
@@ -4,14 +4,24 @@ y = [0,0,1,1]';
 
 rng(1); % seed the random number generator
 weights = rand(3,1); % there are only two hidden layers initialize one set of weights
+err_sum = [];
 
-iter = 100;
-for i = 1:iter
-  layer1 = X;
-  layer2 = layer1 * weights;
-  act = sigmoid(layer2);
-  error = y - act
-  % (GT - prediction) * x * a * (1-a)
-  delta = layer1' * (error .* (act .* (1-act)));
-  weights = weights + delta;
+for iter = 10:10:1000
+  for i = 1:iter
+    layer1 = X;
+    layer2 = layer1 * weights;
+    act = sigmoid(layer2);
+    error = y - act;
+    % (GT - prediction) * x * a * (1-a)
+    delta = layer1' * (error .* (act .* (1-act)));
+    weights = weights + delta;
+  end
+  err_sum = [err_sum, sum(error)];
 end
+
+
+plot(10:10:1000, err_sum)
+xlabel('Iteration', 'FontSize',10)
+ylabel('Total Error', 'FontSize',10)
+title('Effect of Back Propagation','FontSize',12)
+print('demo_nnet_1','-dpng')
