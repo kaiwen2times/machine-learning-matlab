7b5a2e7f8f1e52831f2db6cefbd6f864007f5690
KaiwenZ <kaiwen.zheng1939@gmail.com>
2017-12-05 22:40:22 -0500

    need to change computer.

---
+++ hw8/hw8.m
@@ -0,0 +1,47 @@
+clear;
+clc;
+close all;
+%==========================================================================
+% question 1
+%==========================================================================
+% filter 3x3x3
+% output 16x16x96
+
+
+%==========================================================================
+% question 2
+%==========================================================================
+% output 16x16x96
+
+
+%==========================================================================
+% question 3a
+%==========================================================================
+% a. How many activation layers are present in this network?
+% 2, one ReLU, one softmax
+% b. How many Pooling layers are present in this network?
+% 1
+% c. What are the filter dimensions and number of filters in the first Convolutional layer?
+% 5x5x20
+% d. What is the size of the image after applying the Convolutional layer?
+% 20x20x20
+% e. What is the batch size we are using in this model?
+% 10
+% f. What is the learning rate value used in this model?
+% 0.001
+
+
+%==========================================================================
+% question 3b
+%==========================================================================
+
+
+
+
+
+
+
+
+
+
+
