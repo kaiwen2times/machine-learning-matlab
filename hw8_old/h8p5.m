e0d45feeb12f9292ab08292fc53f7aa7ae889a2d
Kaiwen Zheng <kzheng@constantcontact.com>
2018-01-29 10:02:28 -0500

    remove unused files.

--- hw8_old/h8p5.m
+++
@@ -1,6 +0,0 @@
-load 'imagenet-googlenet-dag.mat' %if it's in the path
-
-for i=1:64
-    subplot(8,8,i)
-    imshow(params(1).value(:,:,:,i));
-end;
--- hw8_old/proj.m
+++
@@ -1,3 +0,0 @@
-
-
-[net,info] = cifar_cnn_cmpe677hw8(cifar_cnn_initialize_sample);
